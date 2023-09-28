43ade1cd38c44c5002b55b289d8cf1999be0255a
frank gimeno <frankgimeno3@gmail.com>
2023-09-28 07:01:38 +0200

    añado menu desplegable funcional

--- components/home/Navbar.tsx
+++ components/home/Navbar.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from "next/image";
 import { useRouter } from "next/router";
 
 interface NavbarProps {
   currentPage: string;
   setCurrentPage: (pageName: string) => void;
+  isMenuOpen: any
+  setIsMenuOpen: any
 }
 
-const Navbar: React.FC<NavbarProps> = ({ currentPage, setCurrentPage }) => {
+const Navbar: React.FC<NavbarProps> = ({ currentPage, setCurrentPage, isMenuOpen,
+  setIsMenuOpen }) => {
   const router = useRouter();
 
   const handleHomeClick = () => {
     setCurrentPage('Home')
+    setIsMenuOpen(false)
   };
 
   const handleQuienesClick = () => {
@@ -30,58 +34,67 @@ const Navbar: React.FC<NavbarProps> = ({ currentPage, setCurrentPage }) => {
     setCurrentPage("Contacto")
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log(isMenuOpen)
+  }
+
   return (
-    <nav className="fixed top-0 left-0 w-full z-50">
-      <div className="flex flex-row justify-between z-10 relative">
-        <button
-          className="text-yellow-500 px-5 py-3 m-2 ml-5 text-lg"
-          onClick={handleHomeClick}
-        >
-          <span className='font-bold'>giru</span>.es
-        </button>
-        <div className="hidden sm:block flex flex-row text-right pr-4 py-2 w-full text-gray-800 font-bold">
+    <>
+      <nav className="fixed top-0 left-0 w-full z-50">
+        <div className="flex flex-row justify-between z-10 relative">
           <button
-            className={`mx-4 text-sm ${currentPage === 'Home' ? 'text-white' : 'text-yellow-500'}`}
+            className="text-yellow-500 px-5 py-3 m-2 ml-5 text-lg"
             onClick={handleHomeClick}
           >
-            Home
+            <span className='font-bold'>giru</span>.es
           </button>
-          <button
-            className={`mx-4 text-sm ${currentPage === 'QuienesSomos' ? 'text-white' : 'text-yellow-500'}`}
-            onClick={handleQuienesClick}
-          >
-            Nosotros
-          </button>
-          <button
-            className={`mx-4 text-sm ${currentPage === 'Servicios' ? 'text-white' : 'text-yellow-500'}`}
-            onClick={handleServiciosClick}
-          >
-            Servicios
-          </button>
-          <button
-            className={`mx-4 text-sm ${currentPage === 'Contacto' ? 'text-white' : 'text-yellow-500'}`}
-            onClick={gotocontacts}
-          >
-            Contáctanos
-          </button>
-          <button
-            className="bg-yellow-500 text-black rounded-lg my-2 px-3 ml-3 md:mx-4 py-2 text-sm font-light"
-            onClick={handleIniciarSesionClick}
-          >
-            Iniciar Sesión
+          <div className="hidden sm:block flex flex-row text-right pr-4 py-2 w-full text-gray-800 font-bold">
+            <button
+              className={`mx-4 text-sm ${currentPage === 'Home' ? 'text-white' : 'text-yellow-500'}`}
+              onClick={handleHomeClick}
+            >
+              Home
+            </button>
+            <button
+              className={`mx-4 text-sm ${currentPage === 'QuienesSomos' ? 'text-white' : 'text-yellow-500'}`}
+              onClick={handleQuienesClick}
+            >
+              Nosotros
+            </button>
+            <button
+              className={`mx-4 text-sm ${currentPage === 'Servicios' ? 'text-white' : 'text-yellow-500'}`}
+              onClick={handleServiciosClick}
+            >
+              Servicios
+            </button>
+            <button
+              className={`mx-4 text-sm ${currentPage === 'Contacto' ? 'text-white' : 'text-yellow-500'}`}
+              onClick={gotocontacts}
+            >
+              Contáctanos
+            </button>
+            <button
+              className="bg-yellow-500 text-black rounded-lg my-2 px-3 ml-3 md:mx-4 py-2 text-sm font-light"
+              onClick={handleIniciarSesionClick}
+            >
+              Iniciar Sesión
+            </button>
+          </div>
+          <button className=' mr-10 mt-4 sm:hidden '
+            onClick={toggleMenu}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 48 48">
+              <path
+                fill="#FFF"
+                d="M12 14h24c1.1 0 2-.9 2-2s-.9-2-2-2H12c-1.1 0-2 .9-2 2s.9 2 2 2zm0 8h24c1.1 0 2-.9 2-2s-.9-2-2-2H12c-1.1 0-2 .9-2 2s.9 2 2 2zm0 8h24c1.1 0 2-.9 2-2s-.9-2-2-2H12c-1.1 0-2 .9-2 2s.9 2 2 2z"
+              />
+            </svg>
           </button>
         </div>
-        <div className=' mr-10 mt-4   sm:hidden '>
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 48 48">
-            <path
-              fill="#FFF"
-              d="M12 14h24c1.1 0 2-.9 2-2s-.9-2-2-2H12c-1.1 0-2 .9-2 2s.9 2 2 2zm0 8h24c1.1 0 2-.9 2-2s-.9-2-2-2H12c-1.1 0-2 .9-2 2s.9 2 2 2zm0 8h24c1.1 0 2-.9 2-2s-.9-2-2-2H12c-1.1 0-2 .9-2 2s.9 2 2 2z"
-            />
-          </svg>
-        </div>
-      </div>
-      <div className="absolute inset-0 opacity-80 bg-black"></div>
-    </nav>
+        <div className="absolute inset-0 opacity-80 bg-black"></div>
+      </nav>
+
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,19 @@ import Floating from "../components/home/Floating";
 import React, { useState } from "react";
 import Iniciarsesion from "./login";
 import Contacto from "../components/home/homebuttons/contactanos";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const [currentPage, setCurrentPage] = useState<string>("Home");
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const router = useRouter();
 
   const handlePageChange = (pageName: string) => {
     setCurrentPage(pageName);
   };
   let currentComponent;
   let componentclass
-  
+
   switch (currentPage) {
     case "Home":
       currentComponent = <Inicio />;
@@ -45,24 +48,64 @@ export default function Home() {
   }
 
 
-   const shouldRenderLanding =
+  const shouldRenderLanding =
     currentPage === "Home" || currentPage === "QuienesSomos" || currentPage === "Servicios";
 
+
+  const handleQuienesClick = () => {
+    setCurrentPage('QuienesSomos')
+    setIsMenuOpen(false)
+  };
+
+  const handleServiciosClick = () => {
+    setCurrentPage('Servicios')
+    setIsMenuOpen(false)
+  };
+
+  const handleIniciarSesionClick = () => {
+    router.push('/login');
+    setIsMenuOpen(false)
+  };
+
+  const gotocontacts = () => {
+    setCurrentPage("Contacto")
+    setIsMenuOpen(false)
+  };
+   
   return (
-    
-      <div className="flex flex-col bg-slate-50  ">
-        <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+    <>
+      <div className="flex flex-col bg-slate-50 absolute  inset-0 " >
+        <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage}
+          isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
 
         {shouldRenderLanding && <Landing />}
 
         <div className={componentclass}>{currentComponent}</div>
         <Footer onPageChange={handlePageChange} />
-           {currentPage !== "Contacto" && (
-            <div className="fixed bottom-2 right-0  p-3">
-              <Floating setCurrentPage={setCurrentPage}/>
-            </div>
-          )}
-       </div>
+        {currentPage !== "Contacto" && (
+          <div className="fixed bottom-2 right-0  p-3">
+            <Floating setCurrentPage={setCurrentPage} />
+          </div>
+        )}
+        <div className={isMenuOpen ? 
+          'fixed top-0 right-0 flex flex-col text-gray-800 z-50 bg-white bg-opacity-90   mt-16 border border-gray-100' : 'hidden'}>
+          <ul className='flex flex-col text-lg text-gray-600 w-screen '>
+            <button className='p-8 py-14 w-full border border-gray-100 bg-white bg-opacity-80' onClick={handleQuienesClick}>
+              Nosotros
+            </button>
+            <button className='p-8 py-16 w-full border border-gray-100 bg-white bg-opacity-80' onClick={handleServiciosClick}>
+              Servicios
+            </button>
+            <button className='p-8 py-16 w-full border border-gray-100 bg-white bg-opacity-80' onClick={gotocontacts}>
+              Contáctanos
+            </button>
+            <button className='p-8 py-16 w-full border border-gray-100 bg-white bg-opacity-80' onClick={handleIniciarSesionClick}>
+              Iniciar Sesión
+            </button>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
