3c325b482f96bbb0cd451a153c2e7f1eda439c6f
frank gimeno <frankgimeno3@gmail.com>
2023-09-28 04:53:54 +0200

    ajusto footer

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 
 interface FooterProps {
-   onPageChange: (pageName: string) => void;
+  onPageChange: (pageName: string) => void;
 }
 const Footer: React.FC<FooterProps> = ({ onPageChange }) => {
   const router = useRouter();
@@ -13,26 +13,25 @@ const Footer: React.FC<FooterProps> = ({ onPageChange }) => {
   };
 
   return (
-    <footer className="bg-gray-900 py-12 relative lg:px-24 text-sm">
-
-<div className="lg:mx-24 text-white relative z-10">
-        <div className="flex flex-row  items-top justify-between align-top">
+    <footer className="bg-gray-900 py-12 relative lg:px-24 text-xs sm:text-sm">
+      <div className="mx-10 lg:mx-24 text-white relative z-10">
+        <div className="flex flex-col sm:flex-row  items-top justify-between align-top">
           <div className="mb-4 flex-1">
             <h3 className="text-md font-semibold">Sobre Nosotros</h3>
             <p className="text-md mt-5">
-            Giru.es es una agencia de desarrolladores web con sede en Barcelona, comprometidos en ofrecer soluciones de desarrollo de software de alta calidad. Nuestro equipo está formado por expertos en diferentes tecnologías y estamos dedicados a ayudar a nuestros clientes a alcanzar sus objetivos tecnológicos.
+              Giru.es es una agencia de desarrolladores web con sede en Barcelona, comprometidos en ofrecer soluciones de desarrollo de software de alta calidad. Nuestro equipo está formado por expertos en diferentes tecnologías y estamos dedicados a ayudar a nuestros clientes a alcanzar sus objetivos tecnológicos.
             </p>
             <p className="text-md">
-            Actualmente, formamos parte del grupo PROPORCION3,S.A. medios de comunicación .</p>
+              Actualmente, formamos parte del grupo PROPORCION3,S.A. medios de comunicación .</p>
           </div>
-          <div className="mb-4  flex-1 text-center" >
+          <div className="mb-4  flex-1 text-center py-10 sm:py-1" >
             <h3 className="text-md font-semibold">Enlaces Rápidos</h3>
             <div className="  flex flex-col">
-                 <button className="mt-5" onClick={() => redirection('Home')} >Inicio</button>
-                 <button className="mt-5" onClick={() => redirection('QuienesSomos')} >Nosotros</button>
-                 <button className="mt-5" onClick={() => redirection('Servicios')} >Servicios</button>
-                 <button className="mt-5" onClick={() => redirection('Contacto')} >Contacto</button>
-             </div>
+              <button className="mt-5" onClick={() => redirection('Home')} >Inicio</button>
+              <button className="mt-5" onClick={() => redirection('QuienesSomos')} >Nosotros</button>
+              <button className="mt-5" onClick={() => redirection('Servicios')} >Servicios</button>
+              <button className="mt-5" onClick={() => redirection('Contacto')} >Contacto</button>
+            </div>
           </div>
           <div className="flex-1">
             <h3 className="text-md font-semibold">Contáctanos</h3>
@@ -47,8 +46,8 @@ const Footer: React.FC<FooterProps> = ({ onPageChange }) => {
           &copy; 2023 PROPORCION3,S.A. | Todos los derechos reservados
         </div>
       </div>
- 
-       <Image
+
+      <Image
         src="/bananobanner.png"
         alt="Bananobanner"
         className="absolute inset-0 w-full h-full object-cover opacity-20"
